fb82a1d410b7f0a304ee14a9e5ad20c26e2eee49
halomoan filpus simarmata <56300227+moan08@users.noreply.github.com>
2020-01-28 10:21:57 +0700

    contoh
    contoh upload

---
+++ Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Controller {
+
+    private int angka1, angka2, hasil;
+
+ public static void mian(String []args){
+  System.out.println("Hello Word");
+  System.out.println("Hello Word");
+  System.out.println("Hello Word");
+  System.out.println("Hello Word");
+ }
+}
